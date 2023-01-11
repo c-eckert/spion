8b7f28143dc0b9098b89d40a6d39f44154ef87ec
Christian <c98e@outlook.de>
2023-01-12 00:57:07 +0100

    optimize states

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,123 +1,109 @@
 import random
 import streamlit as st
-
-WORDS = {
-        'Berufe': ["Arzt", "Ingenieur", "Lehrer", "Manager", "Polizist", "Krankenschwester", 
-            "Bauarbeiter", "Schauspieler", "Verkäufer", "Rechtsanwalt", "Koch", "Erzieher", 
-            "Architekt", "Journalist", "Informatiker"],
-        'Länder': ["Afghanistan", "Albanien", "Algerien", "Argentinien", "Australien",
-            "Bangladesch", "Belgien", "Bolivien", "Brasilien", "Bulgarien", "Uruguay", 
-            "Kanada", "China", "Kroatien", "Kuba", "Zypern", "Tschechien", "Dänemark", 
-            "Deutschland", "Ägypten", "Estland", "Äthiopien", "Finnland", "Frankreich", 
-            "Griechenland", "Ungarn", "Island", "Österreich", "Israel"],
-        "Orte im Haus" : ["Wohnzimmer", "Küche", "Schlafzimmer", "Bad", "Gästezimmer",
-            "Arbeitszimmer", "Flur", "Balkon", "Garage", "Garten", "Keller", "Dachboden", 
-            "Abstellraum", "Waschküche", "Treppe", "Fenster", "Tür", "Pool"],
-        "Sehenswürdigkeiten" : ["Pyramide", "Taj Mahal", "Kolosseum in Rom", "Chinesische Mauer", 
-            "Niagarafälle", "Schiefe Turm von Pisa", "Freiheitsstatue", "Mount Everest", 
-            "Great Barrier Reef", "Eiffelturm", "Big Ben", "Central Park", "Zugspitze"],
-        "Transport" : ["Auto", "Bus", "Zug", "Flugzeug", "Fahrrad", "Straßenbahn", "Motorrad",
-            "Schiff", "Kanu", "Segelboot", "Surfbrett", "Skateboard", "Inlineskates", "Schlitten",
-            "Snowboard", "Ski", "Helikopter", "Heißluft-Ballon", "Einrad", "E-Bike", "E-Scooter"],
-        "Sport" : ["Fußball", "Basketball", "Tennis", "Volleyball", "Schwimmen", "Laufen", "Boxen",
-            "Golf", "Eishockey", "Handball", "Wrestling", "Klettern", "Surfen", "Segeln", "Marathon", 
-            "American Football", "Dart", "Billard", "Tischtennis", "Schach"],
-        "Einrichtung" : ["Schule", "Universität", "Krankenhaus", "Bibliothek", "Feuerwehr",
-            "Gefängnis", "Museum", "Theater", "Kino", "Fitnessstudio", "Einkaufszentrum", "Supermarkt",
-            "Bank", "Postamt", "Tankstelle", "Hotel", "Restaurant", "Schwimmbäd", "Sauna", "Flughafen", 
-            "Bahnhof"]
-    }
+import json
 
 # Funktion, um das Wort auszuwählen
-def select_word(categories):
+def generate_wordlist(categories, player_count, spy_count):
+    # select word
     selected_words = []
-    for category in categories:
-        selected_words += WORDS[category]
+    with open('words.json') as file:
+        words = json.load(file)
+        for category in categories:
+            selected_words += words[category]
+    selected_word = random.choice(selected_words)
+    # create list
+    word_list = ['Spion'] * spy_count + [selected_word] * (player_count - spy_count)
+    random.shuffle(word_list)
+    return word_list
 
-    return random.choice(selected_words)
+@st.experimental_singleton
+def get_words():
+    with open('words.json') as file:
+        return json.load(file)
 
-def cb_seeing():
-    st.session_state['mini_state'] = "seeing"
+def cb_open():
+    st.session_state['state_assignment'] = "open"
 
-def cb_done():
-    st.session_state['mini_state'] = "done"
-    st.session_state['current_player'] += 1
-    if st.session_state['current_player'] > len(st.session_state['players']):
-        st.session_state['gamestate'] = "done"
+def cb_hide():
+    st.session_state['state_assignment'] = "hidden"
+    if st.session_state['current_player'] >= len(st.session_state['word_list']):
+        st.session_state['state_global'] = "done"
     else:
-        st.session_state['mini_state'] = "waiting"
+        st.session_state['current_player'] += 1
 
-def cb_disable():
+def cb_start():
+    st.session_state['state_assignment'] = "hidden"
+    st.session_state['state_global'] = "assign"
     st.session_state['disabled'] = True
+    st.session_state['current_player'] = 1
+    st.session_state['word_list'] = generate_wordlist(st.session_state['categories'], st.session_state['player_count'], st.session_state['spy_count'])
 
 def cb_new_game():
     st.session_state['disabled'] = False
-    st.session_state['gamestate'] = "ready"
+    st.session_state['state_global'] = "config"
+
 
 def main():
     # initialize states
-    if 'gamestate' not in st.session_state:
-        st.session_state['gamestate'] = "ready"
+    if 'state_global' not in st.session_state:
+        st.session_state['state_global'] = "config" # config --> assign --> done
+
+    if 'state_assignment' not in st.session_state:
+        st.session_state['state_assignment'] = "hidden" # hidden --> open
 
     if 'current_player' not in st.session_state:
         st.session_state['current_player'] = 0
-
-    if 'mini_state' not in st.session_state:
-        st.session_state['mini_state'] = "init"
 
     if 'player_count' not in st.session_state:
         st.session_state['player_count'] = 0
 
     if 'spy_count' not in st.session_state:
         st.session_state['spy_count'] = 0
+
+    if 'categories' not in st.session_state:
+        st.session_state['categories'] = []
     
     if 'disabled' not in st.session_state:
         st.session_state['disabled'] = False
+
+    if 'word_list' not in st.session_state:
+        st.session_state['word_list'] = []
 
     st.title('Spion - das Spiel')
 
     player_count = st.number_input('Wie viele Spieler nehmen teil?', min_value=3, max_value=100, value=8, disabled=st.session_state["disabled"])
     spy_count = st.number_input('Wie viele Spione gibt es?', min_value=1, max_value=player_count, value=3, disabled=st.session_state["disabled"])
-    categories = st.multiselect('Wählen Sie die Kategorien von Wörtern aus', options=WORDS.keys(), default=WORDS.keys(), disabled=st.session_state["disabled"])
+    categories = st.multiselect('Wählen Sie die Kategorien von Wörtern aus', options=get_words().keys(), default=get_words().keys(), disabled=st.session_state["disabled"])
 
-    if st.session_state['gamestate'] == 'ready':
-        if st.button('Spiel starten', on_click=cb_disable):
-            selected_word = select_word(categories)
-            players = ['Spion'] * spy_count + [selected_word] * (player_count - spy_count)
-            random.shuffle(players)
-            st.session_state['players'] = players
-            st.session_state['gamestate'] = "started"
-            st.session_state['current_player'] = 1
-            st.session_state['mini_state'] = "waiting"
-            st.session_state['player_count'] = player_count
-            st.session_state['spy_count'] = spy_count
+    if st.session_state['state_global'] == 'config':
+        st.session_state['player_count'] = player_count
+        st.session_state['spy_count'] = spy_count
+        st.session_state['categories'] = categories
+        with st.sidebar:
+            st.json(get_words())
 
-    else:
+        st.button('Spiel starten', on_click=cb_start)
+
+    elif st.session_state['state_global'] == "assign":
         st.button('Neues Spiel', on_click=cb_new_game)
-
-
-    if st.session_state['gamestate'] == "started":
-        player_count = st.empty()
         st.header("Rollenzuteilung")
         st.subheader(f"Spieler {st.session_state['current_player']} von {st.session_state['player_count']}")
-        player_word = st.empty()
 
-        if st.session_state['mini_state'] == "waiting":
-            player_word.info("Klicke auf 'aufdecken' um dein Wort zu sehen.")
-            st.button("aufdecken", on_click=cb_seeing)
+        if st.session_state['state_assignment'] == "hidden":
+            st.info("Klicke auf 'aufdecken' um dein Wort zu sehen.")
+            st.button("aufdecken", on_click=cb_open)
             
-        if st.session_state['mini_state'] == "seeing":
-            word = st.session_state['players'][st.session_state['current_player'] - 1]
+        elif st.session_state['state_assignment'] == "open":
+            word = st.session_state['word_list'][st.session_state['current_player'] - 1]
             if word == "Spion":
-                player_word.error(word)
+                st.error(word)
             else:
-                player_word.success(word)
-            st.button("zudecken", on_click=cb_done)
+                st.success(word)
+            st.button("zudecken", on_click=cb_hide)
         
-    elif st.session_state['gamestate'] == "done":
-        player_word = st.empty()
+    elif st.session_state['state_global'] == "done":
         st.success(f"Das Spiel kann los gehen! Es gibt {spy_count} Spione und {player_count} Spieler.")
-        st.session_state['disabled'] = False
+        st.button('Neues Spiel', on_click=cb_new_game)
 
 
 if __name__ == '__main__':
