83683247a6cd733aae777959458c38b06d03dba1
Christian Eckert <c98e@outlook.de>
2023-01-11 09:09:18 +0100

    change

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,36 +1,40 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import random as rd
 
-st.title('Uber pickups in NYC')
+st.title('Spion')
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+no_player = st.number_input('Anzahl an Mitspielern', min_value=3, step=1)
+st.write('The current number is ', no_player)
 
-@st.cache
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+no_spy = st.number_input('Anzahl an Spionen', min_value=1, max_value=no_player-1, step=1)
+st.write('The current number is ', no_spy)
 
-data_load_state = st.text('Loading data...')
-data = load_data(10000)
-data_load_state.text("Done! (using st.cache)")
+cathegories = st.multiselect(
+    "Kathegorien",
+    ["Länder", "Orte", "Verkehrsmittel"],
+    ["Länder", "Orte", "Verkehrsmittel"]
+)
+countries = ["Deutschland", "Schweiz", "Österreich", 
+             "Frankreich", "Belgien", "Niederlande", 
+             "England"]
 
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
+cities = ["Wien", "London", "Paris", "Berlin", "Zürich", "Hamburg"]
 
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
+vehicles = ["Auto", "Zug", "Straßenbahn", "Flugzeug", "Schiff", "Luftschiff", "Hubschrauber"]
 
-# Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+words = []
+if "Länder" in cathegories:
+    words += countries
+if "Orte" in cathegories:
+    words += cities
+if "Verkehrsmittel" in cathegories:
+    words += vehicles
 
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
+print(words)
+if len(words) > 0:
+    game_word = rd.choice(words)
+
+    print(game_word)
+    start = st.button("Start")
